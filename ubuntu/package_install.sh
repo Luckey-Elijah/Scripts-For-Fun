1d92c36b09b830afb4884d86deace0bec0d08696
Elijah Luckey <luckeyelijah112@gmail.com>
2019-05-28 13:53:41 -0400

    add remote non apt applications: draw.io, vs-code, sendanywhere, and teamviwer

--- ubuntu/package_install.sh
+++ ubuntu/package_install.sh
@@ -15,4 +15,31 @@ sudo apt install $(cat packages.txt)
 
 # Installs from other sources
 
-# VS Code build from source
+# Draw.io
+
+wget https://github.com/jgraph/drawio-desktop/releases/download/v9.3.1/draw.io-amd64-9.3.1.deb 
+sudo dpkg -i draw.io-amd64-9.3.1.deb
+rm draw.io-amd64-9.3.1.deb
+
+# Minecraft Launcher
+
+
+
+# VS-Code
+
+sudo snap install vscode --classic
+
+# Sendanywhere
+
+wget https://update.send-anywhere.com/linux_downloads/sendanywhere_latest_amd64.deb
+sudo dpkg -i sendanywhere_latest_amd64.deb
+rm sendanywhere_latest_amd64.deb
+
+# Teamviewer
+
+wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+sudo dpkg -i teamviewer_amd64.deb
+rm teamviewer_amd64.deb
+
+sudo apt autoremove
+sudo apt autoclean
