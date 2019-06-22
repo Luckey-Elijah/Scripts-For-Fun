ee961cfcaa17fbdfa0e88503dca4adff15ce09ca
Elijah Luckey <luckeyelijah112@gmail.com>
2019-06-22 16:46:58 -0400

    empty trash clears contents of trashcan with a little more funcitonality

---
+++ MacOS/empty_trash.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+trashPath="~/.Trash"
+numItems=`ls -l ~/Trash | wc -l`
+
+# checks if can is empty
+function canIsEmpty() {
+    echo "No need to take out the trash, your bin is already empty."
+    exit 0
+}
+
+# removes regular files in bin, leaving all directories
+function rmOnlyFiles() {
+    echo "Removing all regular files in your bin."
+    find "$trashPath" -type f -delete
+    exit 0
+}
+
+# will only remove the directories in the bin
+function rmOnlyDir() {
+    echo "Removing all directories in your bin."
+    rm -rf "$trashPath"
+    exit 0
+}
+
+# clears EVERYTHING in bin
+function emptyAllTrash(){
+    echo "Emptying your trash bin."
+    rm -rf "$trashPath"
+    exit 0
+}
+
+# if the bin is empty then returns true
+function isCanEmpty() {
+    if [ "$numItems" == 0 ]
+    then
+        return true
+    fi
+    return false
+}
+
+# driver code for emptying the trash
+function main() {
+    if isCanEmpty 
+    then
+        canIsEmpty
+    else
+        if [ $1 == "file" -o $1 == "files" -o $1 == "f" ]
+        then
+            rmOnlyFiles
+        elif [ $1 == "dir" -o $1 == "directory" -o $1 == "d" ]
+        then
+            rmOnlyDir
+        else 
+            emptyAllTrash
+        fi
+    fi
+}
+
+main
