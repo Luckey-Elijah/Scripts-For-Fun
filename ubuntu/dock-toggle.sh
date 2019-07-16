6ebb04448a16a4abc5b2beafa41336b8e4d11c58
Elijah Luckey <luckeyelijah112@gmail.com>
2019-07-16 17:56:24 -0400

    short script added for remove gnome dock

---
+++ ubuntu/dock-toggle.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Uncomment the line for removing or adding the dock."
+
+# remove the dock
+# sudo apt remove gnome-shell-extension-ubuntu-dock
+
+# add the dock
+# sudo apt install gnome-shell-extension-ubuntu-dock
