e7d2aadf445279ff995daf36fdd87bc542124785
Elijah Luckey <luckeyelijah112@gmail.com>
2019-08-12 10:58:37 -0400

    adds descriptive comments to "backup and delete"

--- windows/backup_and_delete.ps1
+++ windows/backup_and_delete.ps1
@@ -6,35 +6,39 @@
 $LOGFILE = "C:\Scripts\delete_files_log.log"
 $SOURCEPATH = ""
 $DESTINATIONPATH = ""
-$FILEAGE = "14"
+$FILEAGE = "14" # Default is 14 days
 
-# Function that copies files from $SOURCE
+# Function that copies files from the source path into the destination path
 function Copy-Files {
     Copy-Item (Join-Path $SOURCEPATH "*") $DESTINATIONPATH -Exclude (Get-ChildItem $DESTINATIONPATH)
 }
 
+# Function that removes the items in the source path that are older than the file age variable
 function Remove-SourceFiles {
     Get-ChildItem -Path "$SOURCEPATH" -Recurse | Where-Object CreationTime -lt  (Get-Date).AddDays(-$FILEAGE) | Remove-Item -Force -Recurse *>&1 >> $LOGFILE
 }
+
+# Function that writes the script "copy" status to the log file
 function Write-StatusCopy {
-    Write-Output "----------------------------------------" >> $LOGFILE
+    Write-Output "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -" >> $LOGFILE
     Write-Output "Path of source:               " $SOURCEPATH >> $LOGFILE
     Write-Output "Path of destination:          " $DESTINATIONPATH >> $LOGFILE
     Write-Output "Started backup on:            " Get-Date >> $LOGFILE
-
 }
 
+# Function that wrte the script "remove" status to the log file
 function Write-StatusRemove {
     Write-Output "Path of source to be removed: " $SOURCEPATH
     Write-Output "Started removing on:          " Get-Date >> $LOGFILE
 }
 
-
+# Function that writes the script "ending" status to the log file
 function Close-Log {
     Write-Output "Finished removing on:         " Get-Date >> $LOGFILE
     Write-Output
 }
 
+# Calling the functions to execute the script
 Write-StatusCopy
 Copy-Files
 
