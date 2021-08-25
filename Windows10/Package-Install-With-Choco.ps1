029ef64d6b37a880a528284d4a0f4eea76137075
Elijah Luckey <luckeyelijah112@gmail.com>
2021-08-25 14:33:41 -0400

    Moved choco installer

--- Windows10/Package-Install-With-Choco.ps1
+++ Windows10/Package-Install-With-Choco.ps1
@@ -1,7 +1,7 @@
 # Installing chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-# this allows items to be install without the "-y" flag. 
+# this allows items to be install without the "-y" flag.
 choco feature enable -n=allowGlobalConfirmation
 
 refreshenv
